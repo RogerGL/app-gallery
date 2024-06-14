88767a369077e72bb51a6bc7d2eb97389dceb07a
Roger Guimarães <86247840+RogerGL@users.noreply.github.com>
2024-06-14 10:05:57 -0300

    Update Dashboard.tsx

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -9,12 +9,12 @@ export default function Dashboard({ auth }: PageProps) {
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Sistema Eventos</h2>}
         >
-            <Head title="Sistema Eventos" />
+            <Head title="Sistema Fotos de Eventos" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Sistema de eventos onde é disponibilizado tanto a galeria e criação, consulte a informação sobre proxímo evento enviado por e-mail.</div>
+                        <div className="p-6 text-gray-900">Sistema de eventos onde é disponibilizado tanto a galeria e criação, consulte a informação sobre proxímo evento enviado.</div>
                     </div>
                 </div>
             </div>
