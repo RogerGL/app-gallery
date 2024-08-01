4373421f98eea926b7c09cb450350486bfa3cf8f
Roger Guimarães <86247840+RogerGL@users.noreply.github.com>
2024-08-01 15:14:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,9 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
-    ],
+    ],server: {
+        hmr: {
+            host: 'localhost',
+        },
+  },
 });
